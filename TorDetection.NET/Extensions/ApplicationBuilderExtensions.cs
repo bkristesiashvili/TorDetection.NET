4e2c9270859f4569fce74c772a2cacdab6cc0058
Besik Kristesiashvili <61345934+bkristesiashvili@users.noreply.github.com>
2021-07-09 20:28:30 +0400

    Update ApplicationBuilderExtensions.cs

--- TorDetection.NET/Extensions/ApplicationBuilderExtensions.cs
+++ TorDetection.NET/Extensions/ApplicationBuilderExtensions.cs
@@ -14,7 +14,7 @@ namespace TorDetection
         /// Protects the application accessing from TOR network
         /// </summary>
         /// <param name="this"></param>
-        /// <param name="redirectUrl">redirect URL where the request will be redirected if access will be presented.</param>
+        /// <param name="redirectUrl">where the request will be redirected if access will be presented.</param>
         /// <returns></returns>
         public static IApplicationBuilder UseTorDetection(this IApplicationBuilder @this,
             string redirectUrl)
